5997ae700b4f62406039e28026758ec54c48d5b2
Marco Romagna <maromagna@gmail.com>
2023-05-15 15:08:12 -0300

    estilizando SideBar

--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar/indes'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src='https://github.com/MARomagna.png' alt='' />
-    <Titulo fontSize={20}>Marco Romagna</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Marco Romagna</Titulo>
+      <Paragrafo fontSize={16} tipo='secundario'>
+        MAromagna
+      </Paragrafo>
+      <Descricao tipo='principal' fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
