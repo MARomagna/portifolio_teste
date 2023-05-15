ebe61c723cca088b56c6585328913bc1a6d91852
Marco Romagna <maromagna@gmail.com>
2023-05-15 14:16:24 -0300

    secao de projetos

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,12 @@
+import Paragrafo from '../Paragrafo'
+import Titulo from '../titulo'
+import { Card, LinkBotao } from './styles'
+const Projeto = () => (
+  <Card>
+    <Titulo>Projeto Lista de Tarefas</Titulo>
+    <Paragrafo tipo='secundario'>Lista de Tarefas feita com VueJS</Paragrafo>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+
+export default Projeto
--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,7 +1,35 @@
+import Projeto from '../../components/Projeto'
 import Titulo from '../../components/titulo'
+import { Lista } from './styles'
 const Projetos = () => (
   <section>
     <Titulo fontSize={16}>Projetos</Titulo>
+    <Lista>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+    </Lista>
   </section>
 )
 export default Projetos
